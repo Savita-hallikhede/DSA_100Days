3d189ef057cf073698c661d30f14b12ea83ab7fd
savita <savitahallikhede11@gmail.com>
2026-09-12 22:16:26 +0530

    Day5

--- DSA_100Days/Day5/Array/SortingArray.cs
+++ DSA_100Days/Day5/Array/SortingArray.cs
@@ -14,7 +14,25 @@ namespace DSA_100Days.Day5.Array
         {
             int[] arr = { 4, 6, 2, 1, 5, 9, 0, 5, 7 };
 
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
 
+            Console.WriteLine("Sorted Array:");
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
         }
     }
 }
